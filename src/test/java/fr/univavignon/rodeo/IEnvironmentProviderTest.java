acffc9ab07e25bdb1c2816e4990b8f2c28f1eeb1
elkhattab <elkhattab.mahmoud@gmail.com>
2018-04-09 15:03:43 +0200

    unitTest case for IAnimal interface

--- src/test/java/fr/univavignon/rodeo/IEnvironmentProviderTest.java
+++ src/test/java/fr/univavignon/rodeo/IEnvironmentProviderTest.java
@@ -20,7 +20,7 @@ public class IEnvironmentProviderTest {
 	}
 	@Test 
 	public void testGetEnvironment() {
-		getIEnvironmentProviderInstance();
+		getIEnvironmentInstance();
 		Mockito.when(IEnvProvider.getEnvironment("")).thenReturn(null);
 		assertEquals(IEnvProvider.getEnvironment(""), null);
 	}
