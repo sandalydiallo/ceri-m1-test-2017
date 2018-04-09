408f480eb0432df13ff8dd9b965f4a4261c00f80
elkhattab.mahmoud@gmail.com <elkhattab.mahmoud@gmail.com>
2018-04-09 18:10:32 +0200

    The implementation of the ISpecie and IEnvironment interfaces

--- src/main/java/fr/univavignon/rodeo/implementation/Environment.java
+++ src/main/java/fr/univavignon/rodeo/implementation/Environment.java
@@ -4,32 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 import fr.univavignon.rodeo.api.IAnimal;
+import fr.univavignon.rodeo.api.IEnvironment;
 import fr.univavignon.rodeo.api.ISpecie;
 
-public class Specie implements ISpecie {
+public class Environment implements IEnvironment {
 
 	private String name ; 
 	private int area ; 
-	private List<IAnimal> animals ; 
+	private List<ISpecie> species ; 
 	
-	public Specie(String name , int area ){
+	public Environment(String name , int area ){
 		this.name = name ; 
 		this.area = area ; 
-		this.animals= new ArrayList<IAnimal>(10) ; 
+		this.species= new ArrayList<ISpecie>(10) ; 
 	}
+	
 	public String getName() {
 		// TODO Auto-generated method stub
 		return this.name;
 	}
 
-	public int getArea() {
+	public int getAreas() {
 		// TODO Auto-generated method stub
 		return this.area;
 	}
 
-	public List<IAnimal> getAnimals() {
+	public List<ISpecie> getSpecies() {
 		// TODO Auto-generated method stub
-		return this.animals;
+		return this.species;
 	}
 
 }
