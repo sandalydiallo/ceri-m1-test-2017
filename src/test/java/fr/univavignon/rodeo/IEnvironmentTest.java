6793f123434af5528c41f5122725dfb401aa214f
EL KHATTAB mahmoud <elkhattab.mahmoud@gmail.com>
2018-04-09 14:40:39 +0200

    unitTest case for IAnimal interface

--- src/test/java/fr/univavignon/rodeo/IEnvironmentTest.java
+++ src/test/java/fr/univavignon/rodeo/IEnvironmentTest.java
@@ -6,10 +6,21 @@ import static org.junit.Assert.*;
 import junit.framework.TestCase;
 
 import org.junit.* ; 
+import org.mockito.Mockito;
+
+import fr.univavignon.rodeo.api.IEnvironment;
 
 
 
 public class IEnvironmentTest {
+	
+	IEnvironment IEnvProvider ; 
+	@Test
+	public void getIEnvironmentProviderInstance() {
+		//I should create a mock for IAnimal 
+		IEnvProvider = Mockito.mock(IEnvironment.class);
+		
+	}
 	
 	@Test 
 	public void test() {
